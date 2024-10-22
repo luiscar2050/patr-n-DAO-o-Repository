0258e55b3660c82a1bd207cb3b3f5b6f4cf71b6f
luiscar2050 <luis3206162549@gmail.com>
2024-10-22 06:58:11 -0500

    terminado clase main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static libroDAO LibroDAO;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        libroDAO libroDAO = new libroDAOImpl();
+
+        libroDAO.crear(new libro(0, "Spiderman 2", "Stan lee", 2004));
+        libroDAO.crear(new libro(0, "Harry Potter y la piedra filosofal", "J. K. Rowling", 1997 ));
+
+        System.out.println("Lista de libros:");
+        for (libro libro : libroDAO.listar()) {
+            System.out.println(libro);
+        }
+
+        libro libroLeido = libroDAO.leer(1);
+        System.out.println("\nLibro leído: " + libroLeido);
+
+        libroLeido = new libro(libroLeido.getId(), "Spiderman 3", "Stan lee", 2007);
+        libroDAO.actualizar(libroLeido);
+        System.out.println("\nLibro actualizado: " + libroDAO.leer(1));
+
+        libroDAO.eliminar(2);
+        System.out.println("\nLista de libros después de eliminar:");
+        for (libro libro : libroDAO.listar()) {
+            System.out.println(libro);
         }
     }
 }
