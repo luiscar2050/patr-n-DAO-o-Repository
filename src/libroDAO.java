1e4f3076c883433cf2eb5cbd1c495b8058008957
luiscar2050 <luis3206162549@gmail.com>
2024-10-22 06:46:42 -0500

    interfazDAO

--- src/libroDAO.java
+++ src/libroDAO.java
@@ -1,2 +1,9 @@
-public class libroDAO {
+import java.util.List;
+
+public interface libroDAO {
+    void crear(libro libro);
+    libro leer(int id);
+    List<libro> listar();
+    void actualizar(libro libro);
+    void eliminar(int id);
 }
