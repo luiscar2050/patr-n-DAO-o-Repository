3f5d30f09337b0cecf5885dc6f01b0619e7fea4c
luiscar2050 <luis3206162549@gmail.com>
2024-10-22 06:43:58 -0500

    clase libro

---
+++ src/libro.java
@@ -0,0 +1,39 @@
+public class libro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+
+    public libro(int id, String titulo, String autor, int anioPublicacion) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anioPublicacion=" + anioPublicacion +
+                '}';
+    }
+}
